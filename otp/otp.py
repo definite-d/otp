f62e858e0b40d116e3cfa2f6ec5d2458e58ec220
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com.38317208+definite-d@users.noreply.github.com>
2025-06-22 19:09:10 +0100

    Begin work on the `totp` external function.

--- otp/otp.py
+++ otp/otp.py
@@ -9,3 +9,6 @@ def hotp(secret: bytes, counter: int, digits=6) -> str:
         Digit=digits,
         _HMAC_ALGORITHM=AllowedAlgorithms.HMAC_SHA_1,
     )
+
+def totp(secret: str, time_step=30, digits=6, t0=0, digest='sha1') -> str:
+    ...
