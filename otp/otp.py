db7664afaff8af1e8eb343d4c0564fe474bcca8a
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com.38317208+definite-d@users.noreply.github.com>
2025-06-22 19:36:46 +0100

    Add in function to parse TOTP URIs.

--- otp/otp.py
+++ otp/otp.py
@@ -1,6 +1,18 @@
+from typing import TypedDict
+from urllib.parse import parse_qs, unquote, urlparse
+
 from .rfc.common import AllowedAlgorithms
 from .rfc.rfc_4226 import rfc_4226
 from .rfc.rfc_6238 import rfc_6238
+
+
+class URIData(TypedDict):
+    label: str
+    secret: str
+    issuer: str
+    digits: int
+    period: int
+    algorithm: str
 
 
 def hotp(secret: bytes, counter: int, digits=6) -> str:
@@ -20,3 +32,24 @@ def totp(
     algorithm: AllowedAlgorithms = AllowedAlgorithms.HMAC_SHA_256,
 ) -> str:
     return rfc_6238(K=secret, T0=t0, X=period, Digit=digits, algorithm=algorithm)
+
+
+def parse_uri(uri: str) -> URIData:
+    parsed = urlparse(uri)
+    if parsed.scheme != "otpauth" or parsed.netloc != "totp":
+        raise ValueError("Only TOTP URIs are supported.")
+
+    label = unquote(parsed.path[1:])
+    params = parse_qs(parsed.query)
+
+    def get(key, default=None):
+        return params.get(key, [default])[0]
+
+    return URIData(
+        label=label,
+        secret=get("secret"),
+        issuer=get("issuer"),
+        digits=int(get("digits", 6)),
+        period=int(get("period", 30)),
+        algorithm=get("algorithm", "SHA1").lower(),
+    )
