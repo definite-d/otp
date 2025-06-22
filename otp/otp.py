ad33f43eced596624d0d868da9ec024cc02e46b6
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com.38317208+definite-d@users.noreply.github.com>
2025-06-22 16:46:10 +0100

    Adjust docstring, and add basic validation

--- otp/otp.py
+++ otp/otp.py
@@ -10,7 +10,9 @@ class AllowedAlgorithms(Enum):
 # HOTP (RFC 4226) - https://www.ietf.org/rfc/rfc4226.txt
 def _rfc_4226(C: bytes, K: bytes, Digit: int = 6, HMAC: AllowedAlgorithms = AllowedAlgorithms.HMAC_SHA_1) -> int:
     """
-    Implementation of the HOTP algorithm, following RFC 4226.
+    Implementation of the HOTP algorithm, following RFC 4226
+    (with the HMAC parameter being the only deviation from the spec).
+
     :param C:  8-byte counter value, the moving factor.
         This counter MUST be synchronized between the HOTP generator (client)
          and the HOTP validator (server).
@@ -20,7 +22,12 @@ def _rfc_4226(C: bytes, K: bytes, Digit: int = 6, HMAC: AllowedAlgorithms = Allo
     :param HMAC: HOTP hash function; HMAC_SHA_1 by default, spec-compliant.
     :return D: D is a number in the range 0...10^{Digit}-1
     """
-    ...
+
+    # Validation
+    if Digit < 6:
+        raise ValueError('Digit must be >= 6')
+    if HMAC not in AllowedAlgorithms:
+        raise ValueError('HMAC must be one of: ' + ", ".join(AllowedAlgorithms))
 
 
 def hotp():
