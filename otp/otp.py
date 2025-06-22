bcb182c2e824b9f1bb59681ff8ad40ac4fc3dc5b
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com.38317208+definite-d@users.noreply.github.com>
2025-06-22 19:08:23 +0100

    Complete the `hotp` external function.

--- otp/otp.py
+++ otp/otp.py
@@ -1,5 +1,11 @@
+from .rfc.common import AllowedAlgorithms
 from .rfc.rfc_4226 import rfc_4226
 
 
-def hotp():
-    return rfc_4226()
+def hotp(secret: bytes, counter: int, digits=6) -> str:
+    return rfc_4226(
+        C=(counter.to_bytes(8, "big")),
+        K=secret,
+        Digit=digits,
+        _HMAC_ALGORITHM=AllowedAlgorithms.HMAC_SHA_1,
+    )
