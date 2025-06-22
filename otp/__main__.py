34f6b9905d10d901c2e49a8ea12c4d01f7a95f27
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com.38317208+definite-d@users.noreply.github.com>
2025-06-22 19:27:13 +0100

    Define parser

--- otp/__main__.py
+++ otp/__main__.py
@@ -1,5 +1,25 @@
 from .otp import totp
 from argparse import ArgumentParser
 
-if __name__ == '__main__':
+parser = ArgumentParser(
+    prog="OTP",
+    description="Takes a URI for an HOTP or TOTP, and updates a display of the OTP code presently.",
+    epilog="Copyright (C) Afam-Ifediogor, U. Divine.",
+)
+parser.add_argument(
+    "URI",
+    metavar="URI",
+    nargs="?",
+    type=str,
+    help="The URI for an HOTP or TOTP",
+)
+parser.add_argument(
+    "-r",
+    "--refresh-interval",
+    type=int,
+    default=1,
+    help="How often to refresh the OTP code display.",
+)
+
+if __name__ == "__main__":
     ...
